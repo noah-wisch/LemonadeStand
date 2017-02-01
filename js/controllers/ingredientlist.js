cdc90dc54d9e97d077508a184d493cd8d8cb8ae8
Noah Wisch <noah.wisch@gmail.com>
2017-02-01 12:59:45 -0500

    added return to service

--- js/controllers/ingredientlist.js
+++ js/controllers/ingredientlist.js
@@ -1,12 +1,7 @@
 
-// module.exports = {
-//     name: 'IngredientListController',
-//     func($scope, IngredientService) {
-//         $scope.buy = (what) => {
-//             console.log(`Buying ${what.name}`);
-//             if (what.count > 0) {
-//                 what.count--;
-//             }
-//         };
-//     },
-// }
+module.exports = {
+    name: 'IngredientListController',
+    func($scope, IngredientService) {
+        $scope.ingredients = IngredientService.allIngredients();
+    },
+}
--- js/services/ingredientservice.js
+++ js/services/ingredientservice.js
@@ -8,5 +8,10 @@ module.exports = {
             { name: 'Ice', count: 1, price: 0.50 },
             { name: 'Cups', count: 1, price: 0.10 },
         ];
+        return {
+            allIngredients() {
+                return ingredients;
+            }
+        }
     },
 }
